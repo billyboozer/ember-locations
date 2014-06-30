3cabac76b0fd2584d251a34c34d9a1c5fae47670
Billy Boozer <Billy@LivRandom.com>
2014-06-30 14:18:26 -0500

    add ember, ember data, and emblem to gemfile gem 'ember-rails' gem 'ember-source' gem 'emblem-rails'
    Use ember rails to generate a the application structure with
    coffeescript
    
    rails g ember:bootstrap -n App --javascript-engine coffee
    
    Add the proper versions of ember and ember data
    rails g ember:install --tag=v1.5.0 --ember
    rails g ember:install --tag=v1.0.0-beta.7 --ember-data
    
    Add the following lines to your environment files. These tell Ember
    Rails which version of ember.js to use in each environment. The
    production version is minified and has no logging, while the development
    version is not minified and allows for logging.
    
    config.ember.variant = :development
    
    config.ember.variant = :development
    
    config.ember.variant = :production
    
    Ember Rails generates an `application.js.coffee` for us, so lets use
    that. Delete `application.js`. The generated `application.js.coffee`
    requires `jquery`, but not `jquery_ujs`, so make sure to require it
    right below `jquery`:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # config/environments/production.rb
+  config.ember.variant = :production
+
 end
